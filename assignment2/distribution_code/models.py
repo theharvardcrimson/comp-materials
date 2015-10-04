8942a76049fc908b5dfa66bead568e9c7e587afc
jessicamwang <pandy10127@gmail.com>
2015-10-03 23:57:06 -0400

    assignment 2 done? ?

--- assignment2/distribution_code/models.py
+++ assignment2/distribution_code/models.py
@@ -1,4 +1,5 @@
 from datetime import date
+from PIL import Image
 
 
 class Content(object):
@@ -11,7 +12,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = date(year, month, day)
 
         # list of contirbutors
         self.contributors = contributors
@@ -20,8 +21,42 @@ class Content(object):
     def show(self):
         raise NotImplementedError
 
-
 # TODO: Define an Article class that extends the Content class
+
+class Article(Content):
+    def __init__(self, year, month, day, contributors, headline, content):
+
+        # Call init in Content class to set datetime, contributors
+        super(Article, self).__init__(year, month, day, contributors)
+        # set headline
+        self.headline = headline
+        # set content
+        self.content = content
+
+    def show(self):
+        print self.headline + " " + self.content
 
 
 # TODO: Define a Picture class that extends the Content class
+
+class Picture(Content):
+    def __init__(self, year, month, day, contributors, title, caption, path):
+
+        # Call init in Content class to set datetime, contributors
+        super(Picture, self).__init__(year, month, day, contributors)
+        # set title
+        self.title = title
+        # set caption
+        self.caption = caption
+        # set path 
+        self.path = path
+
+    def show(self):
+        im = Image.open(self.path)
+
+        print self.title
+        im.show()
+        print self.caption
+
+
+
